5a474ba1df628e66b42362d8261fb2029914c3c5
Sindydejong <sindymdejong@gmail.com>
2025-05-23 23:59:09 +0200

    Clean up + set up app/header/footer

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,68 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="shell">
+    <div class="header">
+      <HeaderComponent/>
     </div>
-  </header>
+    <div class="main">
+      <RouterView />
+      </div>
+      <div class="footer">
+        <FooterComponent/>
+      </div>
+    </div>
 
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style  lang="scss">
+  @import 'styles/style.scss';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  body{
+    margin: 0;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  
+.shell {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas: 
+    "header"
+    "main"
+    "footer";
+  min-height: 100vh;
+  margin: 0;
 }
+
+.header {
+  grid-area: header;
+  background-color: #333;
+  color: white;
+  height: 70px;
+  padding: 1rem;
+  top: 0;
+  position: sticky;
+
+}
+
+.main {
+  grid-area: main;
+  background-color: #f4f4f4;
+  padding: 1rem;
+}
+
+.footer {
+  grid-area: footer;
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+  height: auto;
+  
+}
+
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <h1>Sindy de Jong</h1>
+
+    <input id="toggle" type="checkbox" />
+    <label for="toggle" class="hamburger">
+      <div class="top-bun"></div>
+      <div class="meat"></div>
+      <div class="bottom-bun"></div>
+    </label>
+
+    <div class="nav">
+      <div class="nav-wrapper">
+        <nav>
+          <router-link to="/">HOME</router-link><br />
+          <router-link to="/about">PROJECTS</router-link><br />
+          <router-link to="/contact">ABOUT</router-link>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '../styles/style.scss';
+
+body {
+  background: #336193;
+  color: #6ecddd;
+  font-family: 'Bebas', sans-serif;
+  text-align: center;
+  max-width: 100%;
+  overflow-x: hidden;
+  padding: 0;
+  margin: 0;
+}
+
+h1 {
+  letter-spacing: 1px;
+  word-spacing: 0.15em;
+  font-size: 22px;
+}
+
+#toggle {
+  display: none;
+}
+
+.hamburger {
+  position: absolute;
+  top: 10px;
+  right: 5%;
+  width: 2em;
+  height: 45px;
+  z-index: 5;
+  cursor: pointer;
+}
+
+.hamburger div {
+  position: relative;
+  width: 3em;
+  height: 7px;
+  border-radius: 3px;
+  background-color: #f8982b;
+  margin-top: 8px;
+  transition: all 0.3s ease-in-out;
+}
+
+.nav {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #090e6b;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  opacity: 0;
+  transform: scale(0);
+  transform-origin: top right;
+  transition:
+    transform 0.4s ease-in-out,
+    opacity 0.4s ease-in-out;
+  z-index: 4;
+  pointer-events: none;
+}
+
+#toggle:checked + .hamburger + .nav {
+  transform: scale(1);
+  opacity: 1;
+  pointer-events: auto;
+}
+
+.nav-wrapper {
+  position: relative;
+  overflow-y: auto;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+nav {
+  text-align: center;
+}
+
+nav a {
+  position: relative;
+  text-decoration: none;
+  color: #6ecddd;
+  font-size: 4em;
+  display: inline-block;
+  margin-top: 1.25em;
+  transition: color 0.2s ease-in-out;
+  letter-spacing: 1px;
+}
+
+nav a:before {
+  content: '';
+  height: 0;
+  position: absolute;
+  width: 0.25em;
+  background-color: white;
+  left: -0.5em;
+  transition: all 0.2s ease-in-out;
+}
+
+nav a:hover {
+  color: white;
+}
+
+nav a:hover:before {
+  height: 100%;
+}
+
+/* Hamburger Animation */
+#toggle:checked + .hamburger .top-bun {
+  transform: rotate(-45deg);
+  margin-top: 25px;
+}
+
+#toggle:checked + .hamburger .bottom-bun {
+  opacity: 0;
+  transform: rotate(45deg);
+}
+
+#toggle:checked + .hamburger .meat {
+  transform: rotate(45deg);
+  margin-top: -7px;
+}
+
+#toggle:checked + .hamburger + .nav {
+  top: 0;
+  transform: scale(1);
+}
+</style>
