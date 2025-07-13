4bf8a38e3d9778561478663f72e7866e97120613
Sindydejong <sindymdejong@gmail.com>
2025-07-13 22:00:52 +0200

    Project order added

---
+++ src/data/projectOrder.ts
@@ -0,0 +1,10 @@
+export const projectOrder = [
+  'magic_8ball',
+  'red_hot_chili_peppers',
+  'Grid & Kleur',
+  'Beweging & Interactie',
+  'jamties',
+  'Beeldtaal',
+  'Meesterproef VID',
+  'Typografie'
+];
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,3 +1,5 @@
+import { projectOrder } from './projectOrder'
+
 export interface Project {
   name: string
   image: string
@@ -26,8 +28,7 @@ export interface Project {
 }
 
 export const projects: Project[] = [
- 
-   {
+  {
     name: 'Grid & Kleur',
     image: '/assets/images/neo.png',
     video: 'https://www.youtube-nocookie.com/embed/e0EmUWmckbA?si=2JJsj-AjDEpYgUyH',
@@ -53,9 +54,10 @@ export const projects: Project[] = [
       'De naam en het logo van Neo heb ik zelf ontworpen. Het logo bestaat uit NEO in een sterk capitaal lettertype. Door het logo heen gaan een soort vallende sterren met hun sporen. Neo staat voor nieuw, waardoor ik dit perfect vind passen bij de vallende sterren. die voelen als een nieuw begin.',
     eindproduct:
       'Mijn eindproduct is een digitale interactieve prototype van de website, gepresenteerd in Figma. Dit prototype laat de gebruikerservaring en de visuele elementen van de website zien.',
-    eindproduct_link: 'https://www.figma.com/proto/gI1pxWWKL5Uf52a6DZqdL4/Neo?page-id=0%3A1&node-id=13-2&p=f&viewport=464%2C199%2C0.06&t=YoE1lyUcePMwDvzr-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=13%3A2'
+    eindproduct_link:
+      'https://www.figma.com/proto/gI1pxWWKL5Uf52a6DZqdL4/Neo?page-id=0%3A1&node-id=13-2&p=f&viewport=464%2C199%2C0.06&t=YoE1lyUcePMwDvzr-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=13%3A2'
   },
-    {
+  {
     name: 'Beweging & Interactie',
     image: '/assets/images/beweging.png',
     video: '',
@@ -66,12 +68,10 @@ export const projects: Project[] = [
     duur: '2 weken',
     datum: '20 mei 2025',
     skills: ['Adobe Illustrator', 'Adobe After Effects'],
-    opdracht:
-      `<p>In dit vak heb ik mij verdiept in animatie en motion design als essentieel onderdeel van interactieve interfaces. Ik leerde hoe animatie niet alleen informatie ondersteunt, maar ook interacties vloeiender, begrijpelijker en aantrekkelijker maakt. Tijdens werkcolleges werkte ik aan praktische oefeningen en opdrachten waarbij ik kennismaakte met animatieprincipes, storyboardtechnieken, dynamische iconensets en verschillende tools, waaronder nieuwe AI-tools.</p>
+    opdracht: `<p>In dit vak heb ik mij verdiept in animatie en motion design als essentieel onderdeel van interactieve interfaces. Ik leerde hoe animatie niet alleen informatie ondersteunt, maar ook interacties vloeiender, begrijpelijker en aantrekkelijker maakt. Tijdens werkcolleges werkte ik aan praktische oefeningen en opdrachten waarbij ik kennismaakte met animatieprincipes, storyboardtechnieken, dynamische iconensets en verschillende tools, waaronder nieuwe AI-tools.</p>
       <br>
       <p>De nadruk lag op creatieve ontwikkeling, stijl, en het maken van doordachte keuzes in techniek en vorm. Het einddoel was het realiseren van een animated interface of interactieve animatie met een sterk visueel concept, gepresenteerd als prototype. </p>`,
-       proces:
-      `<p>Mijn concept is dat ik mijzelf namaak in animatie-vorm. De acties die ik uitvoer weerspiegelen mijn skills binnen mijn vakgebied en mijn persoonlijkheid. Hier kies ik voor omdat ik mijn animaties graag in mijn portfolio zou willen gebruiken. <p>
+    proces: `<p>Mijn concept is dat ik mijzelf namaak in animatie-vorm. De acties die ik uitvoer weerspiegelen mijn skills binnen mijn vakgebied en mijn persoonlijkheid. Hier kies ik voor omdat ik mijn animaties graag in mijn portfolio zou willen gebruiken. <p>
       <br>
       <ul>
         <li>Actie 1: Zwaaien</li>
@@ -95,7 +95,8 @@ export const projects: Project[] = [
     eindproduct:
       'Als eindproduct heb ik de animaties weergegeven in een gameboy, waarin het geanimeerde personage (ikzelf) de drie acties uitvoert. Een gameboy leek mij een leuk omhulsel voor mijn animaties, omdat het bijdraagt aan het speelse gevoel. Daarnaast hou ik zelf ook van gamen en vind ik het leuk om een animatie van mij zelf op deze manier te presenteren.',
     eindproduct_video: 'https://www.youtube.com/embed/Dr9z8JcuJYY?si=HOLl3vXevRss560P',
-    eindproduct_link: 'https://www.figma.com/proto/rVaVX2HZNU9ZGnahxSbl1V/animatie?page-id=0%3A1&node-id=35-133&p=f&viewport=173%2C277%2C0.64&t=VqAgfCkyozNFswDn-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=35%3A133'
+    eindproduct_link:
+      'https://www.figma.com/proto/rVaVX2HZNU9ZGnahxSbl1V/animatie?page-id=0%3A1&node-id=35-133&p=f&viewport=173%2C277%2C0.64&t=VqAgfCkyozNFswDn-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=35%3A133'
   },
   {
     name: 'red_hot_chili_peppers',
@@ -165,11 +166,13 @@ Daarna konden wij kiezen om extra aandacht aan de surface laag te besteden of om
     duur: '3 weken',
     datum: '30 juni 2022',
     skills: ['Adobe XD', 'Adobe Illustrator', 'Product development', 'Interactie design'],
-    opdracht: 'Voor dit project moest ik gedurende 3 weken met een groepje aan de slag gaan om een digitaal en/of fysiek product te ontwikkelen die bijdraagt aan een betere wereld. Wij werden op basis van onze gezamenlijke passie, muziek, bij elkaar in de klas gezet en mochten zelf trios vormen.  ',
-    eindproduct: 'JamTies is een interactief muzikaal kletsspel dat spelers aanmoedigt om hun interesses en gevoelens te delen. Voor nu is JamTies gericht om op een festivalsetting gespeeld te worden. Denk hierbij aan festivalgangers die tussen optredens door tijd hebben voor leuke activiteiten en spelletjes.',
-    eindproduct_link: '',
+    opdracht:
+      'Voor dit project moest ik gedurende 3 weken met een groepje aan de slag gaan om een digitaal en/of fysiek product te ontwikkelen die bijdraagt aan een betere wereld. Wij werden op basis van onze gezamenlijke passie, muziek, bij elkaar in de klas gezet en mochten zelf trios vormen.  ',
+    eindproduct:
+      'JamTies is een interactief muzikaal kletsspel dat spelers aanmoedigt om hun interesses en gevoelens te delen. Voor nu is JamTies gericht om op een festivalsetting gespeeld te worden. Denk hierbij aan festivalgangers die tussen optredens door tijd hebben voor leuke activiteiten en spelletjes.',
+    eindproduct_link: ''
   },
-    {
+  {
     name: 'Beeldtaal',
     image: '/assets/images/beeldtaal.png',
     video: '',
@@ -180,8 +183,7 @@ Daarna konden wij kiezen om extra aandacht aan de surface laag te besteden of om
     duur: '2 weken',
     datum: '3 april 2025',
     skills: ['Adobe Photoshop'],
-    opdracht:
-      `<p>Voor deze opdracht ontwierp ik een ethisch verantwoorde poster voor een bewustwordingscampagne, gericht op een specifieke doelgroep. Het doel was om visueel bewustzijn te creëren rond een maatschappelijk relevant thema. </p> 
+    opdracht: `<p>Voor deze opdracht ontwierp ik een ethisch verantwoorde poster voor een bewustwordingscampagne, gericht op een specifieke doelgroep. Het doel was om visueel bewustzijn te creëren rond een maatschappelijk relevant thema. </p> 
       <br> 
       <p> Het ontwerp is gebaseerd op theorieën uit de lessen Semiotiek, Retorica en Gestalt. Door deze theorieën strategisch toe te passen, heb ik een poster ontwikkeld die niet alleen visueel aanspreekt, maar ook overtuigend communiceert. Denk hierbij aan het gebruik van symboliek, contrast, typografische hiërarchie en beeldcompositie om de boodschap kracht bij te zetten. </p>
       <br>
@@ -201,20 +203,19 @@ Daarna konden wij kiezen om extra aandacht aan de surface laag te besteden of om
     duur: '4 weken',
     datum: '16 juni 2025',
     skills: ['Figma', 'Adobe Illustrator', 'Adobe After Effects'],
-    opdracht:
-      `<p>Voor deze opdracht ontwikkelde ik een visuele en interactieve interface rondom een zelfgekozen casus (tekst, beeld, geluid of video). Het doel was om op een persoonlijke en experimentele manier een verhaal te vertalen naar een digitale ervaring. Ik koos zelf hoe ik mijn visie op de inhoud vormgaf en welke middelen ik gebruikte om deze te bemiddelen.</p>
+    opdracht: `<p>Voor deze opdracht ontwikkelde ik een visuele en interactieve interface rondom een zelfgekozen casus (tekst, beeld, geluid of video). Het doel was om op een persoonlijke en experimentele manier een verhaal te vertalen naar een digitale ervaring. Ik koos zelf hoe ik mijn visie op de inhoud vormgaf en welke middelen ik gebruikte om deze te bemiddelen.</p>
       <br>
       <p>De interface mocht functioneel of juist experimenteel zijn, zolang het ontwerp een duidelijke interpretatie liet zien. Geluid speelde een rol in de beleving. Ook integreerde ik kennis uit eerdere vakken in zowel de vormgeving als de interactie van de interface.</p>`,
-    eindproduct:
-       `<p>Voor mijn meesterproef heb ik gekozen om aandacht te geven aan een onderwerp dat mij persoonlijk raakt: drogering in het nachtleven. Na eigen ervaringen en verhalen van vriendinnen, realiseerde ik me hoe onzichtbaar én alledaags dit gevaar is en hoe weinig mensen echt weten wat er gebeurt als het je overkomt.</p>
+    eindproduct: `<p>Voor mijn meesterproef heb ik gekozen om aandacht te geven aan een onderwerp dat mij persoonlijk raakt: drogering in het nachtleven. Na eigen ervaringen en verhalen van vriendinnen, realiseerde ik me hoe onzichtbaar én alledaags dit gevaar is en hoe weinig mensen echt weten wat er gebeurt als het je overkomt.</p>
       <br>
       <p>Ik wilde daarom geen statisch informatief ontwerp, maar een gevoelsmatige ervaring die dichtbij komt. Door middel van horizontale interactie, videobeelden, geluid en typografie, reconstrueer ik stap voor stap een avond uit die eindigt in verwarring, controleverlies en gevaar. Mijn doel is om bewustzijn te vergroten en tegelijkertijd tips meegeven.</p>
       <br>
-      <p> De interface voelt in eerste instantie bijna "gewoon" of herkenbaar, net als het uitgaansleven zelf. Juist daardoor grijpt de omslag naar gevaar je extra aan. Ik geloof dat design niet alleen moet informeren, maar ook moet voelen en dat bewustwording begint met het kunnen inleven. </p>`,  
+      <p> De interface voelt in eerste instantie bijna "gewoon" of herkenbaar, net als het uitgaansleven zelf. Juist daardoor grijpt de omslag naar gevaar je extra aan. Ik geloof dat design niet alleen moet informeren, maar ook moet voelen en dat bewustwording begint met het kunnen inleven. </p>`,
     eindproduct_video: '',
-    eindproduct_link: 'https://www.figma.com/proto/VK2EgoCNi1CzFlD5RWsKx1/Meesterproef?page-id=0%3A1&node-id=98-95&viewport=-2685%2C-1114%2C0.24&t=oYg75ObE3GYCwcXz-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=98%3A95&hotspot-hints=0'
+    eindproduct_link:
+      'https://www.figma.com/proto/VK2EgoCNi1CzFlD5RWsKx1/Meesterproef?page-id=0%3A1&node-id=98-95&viewport=-2685%2C-1114%2C0.24&t=oYg75ObE3GYCwcXz-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=98%3A95&hotspot-hints=0'
   },
-      {
+  {
     name: 'Typografie',
     image: '/assets/images/typespecimen.png',
     video: '',
@@ -229,7 +230,11 @@ Daarna konden wij kiezen om extra aandacht aan de surface laag te besteden of om
       'Een typografie project waarbij ik een type specimen heb gemaakt voor het Broadway font. Dit omvatte het ontwerpen van verschillende typografische elementen en het presenteren van het font in een visueel aantrekkelijke manier.',
     eindproduct:
       'Mijn eindproduct is een digitale interactieve typespecimen waarin het Broadway font op verschillende manieren wordt gepresenteerd. Ik vergelijk het gebruik van vroeger met het gebruik van nu. Dit laat de veelzijdigheid van het font zien en bevat voorbeelden van gebruik in verschillende contexten.',
-    eindproduct_link: 'https://www.figma.com/proto/aQmIdTPCiTE9ey1NLvqdST/Typo?page-id=0%3A1&node-id=97-2&viewport=912%2C57%2C0.07&t=BFebCToSnNdNLI0i-8&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=97%3A2&hide-ui=1'
-  },
-
+    eindproduct_link:
+      'https://www.figma.com/proto/aQmIdTPCiTE9ey1NLvqdST/Typo?page-id=0%3A1&node-id=97-2&viewport=912%2C57%2C0.07&t=BFebCToSnNdNLI0i-8&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=97%3A2&hide-ui=1'
+  }
 ]
+
+export const orderedProjects = projectOrder
+  .map((name) => projects.find((p) => p.name === name))
+  .filter((project): project is Project => project !== undefined);
