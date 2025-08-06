38dd6dac171ac8f77a638047bab7e1e5594b4c31
Sindydejong <sindymdejong@gmail.com>
2025-08-06 17:10:10 +0200

    portrait check aan img slider toegevoegd

--- src/components/ProjectImageSlider.vue
+++ src/components/ProjectImageSlider.vue
@@ -1,11 +1,17 @@
-<!-- filepath: /Users/sindy/Documents/portfolio_2025/src/components/ProjectImageSlider.vue -->
 <template>
   <div class="slider">
     <button class="slider-arrow left" @click="prev" :disabled="current === 0">&lt;</button>
+
     <div class="slider-image">
-      <img :src="images[current].src" :alt="images[current].beschrijving || 'Project afbeelding'" />
+      <img
+        :src="images[current].src"
+        :alt="images[current].beschrijving || 'Project afbeelding'"
+        :class="{ portrait: isPortrait }"
+        @load="handleImageLoad"
+      />
       <p v-if="images[current].beschrijving">{{ images[current].beschrijving }}</p>
     </div>
+
     <button class="slider-arrow right" @click="next" :disabled="current === images.length - 1">&#x3e;</button>
   </div>
 </template>
@@ -18,11 +24,19 @@ const props = defineProps<{
 }>()
 
 const current = ref(0)
+const isPortrait = ref(false)
+
 function prev() {
   if (current.value > 0) current.value--
 }
+
 function next() {
   if (current.value < props.images.length - 1) current.value++
+}
+
+function handleImageLoad(event: Event) {
+  const img = event.target as HTMLImageElement
+  isPortrait.value = img.naturalHeight > img.naturalWidth
 }
 </script>
 
@@ -31,17 +45,29 @@ function next() {
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 1em;
   margin: 0.5em 0;
 }
+
 .slider-image {
   text-align: center;
   max-width: 100%;
 }
+
 .slider-image img {
   max-width: 100%;
-  /* border-radius: 10px; */
+  height: auto;
+  border-radius: 10px;
+  max-height: 80vh;
+  object-fit: contain;
 }
+
+/* Alleen voor verticale afbeeldingen */
+.slider-image img.portrait {
+  max-height: 90svh;
+  width: auto;
+  max-width: none; /* overschrijft max-width zodat width auto werkt */
+}
+
 .slider-arrow {
   color: #ffb405;
   border: none;
@@ -51,18 +77,18 @@ function next() {
   font-size: 2em;
   cursor: pointer;
 }
+
 .slider-arrow:disabled {
   color: grey;
   cursor: default;
 }
 
 @media screen and (max-width: 600px) {
-.slider {
-  gap: 0;
+  .slider {
+    gap: 0;
+  }
+  .slider-arrow {
+    font-size: 1.5em;
+  }
 }
-.slider-arrow {
-  font-size: 1.5em;
-}
-}
-  
 </style>
